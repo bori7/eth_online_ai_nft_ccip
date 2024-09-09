53afeb378244b3618166f32a72177ce195222b69
bori7 <32014655+bori7@users.noreply.github.com>
2024-09-09 01:51:58 +0100

    feat: galadriel's side

--- contracts/scripts/callQuickstart.ts
+++ contracts/scripts/callQuickstart.ts
@@ -4,73 +4,76 @@ import readline from "readline";
 const {ethers} = require("hardhat");
 
 async function main() {
-  const contractABI = [
-    "function initializeDalleCall(string memory message) public returns (uint)",
-    "function lastResponse() public view returns (string)"
-  ];
+    const contractABI = [
+        "function initializeDalleCall(string memory message) public returns (uint)",
+        "function lastResponse() public view returns (string)"
+    ];
 
-  if (!process.env.QUICKSTART_CONTRACT_ADDRESS) {
-    throw new Error("QUICKSTART_CONTRACT_ADDRESS env variable is not set.");
-  }
+    if (!process.env.QUICKSTART_CONTRACT_ADDRESS) {
+        throw new Error("QUICKSTART_CONTRACT_ADDRESS env variable is not set.");
+    }
 
-  const contractAddress = process.env.QUICKSTART_CONTRACT_ADDRESS;
-  const [signer] = await ethers.getSigners();
+    const contractAddress = process.env.QUICKSTART_CONTRACT_ADDRESS;
+    const [signer] = await ethers.getSigners();
 
-  // Create a contract instance
-  const contract = new ethers.Contract(contractAddress, contractABI, signer);
+    // Create a contract instance
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
-  // The content of the image you want to generate
-  const message = await getUserInput();
+    // The content of the image you want to generate
+    const message = await getUserInput();
 
-  // Call the startChat function
-  const transactionResponse = await contract.initializeDalleCall(message);
-  const receipt = await transactionResponse.wait();
-  console.log(`Transaction sent, hash: ${receipt.hash}.\nExplorer: https://explorer.galadriel.com/tx/${receipt.hash}`)
-  console.log(`Image generation started with message: "${message}"`);
+    // Call the startChat function
+    const transactionResponse = await contract.initializeDalleCall(message);
+    const receipt = await transactionResponse.wait();
+    console.log(`Transaction sent, hash: ${receipt.hash}.\nExplorer: https://explorer.galadriel.com/tx/${receipt.hash}`)
+    console.log(`Image generation started with message: "${message}"`);
+    console.log(`Image generation ended with transactionResponse: "${JSON.stringify(transactionResponse)}"`);
+    console.log(`Image generation ended with receipt: "${JSON.stringify(receipt)}"`);
 
-  // loop and sleep by 1000ms, and keep printing `lastResponse` in the contract.
-  let lastResponse = await contract.lastResponse();
-  let newResponse = lastResponse;
 
-  // print w/o newline
-  console.log("Waiting for response: ");
-  while (newResponse === lastResponse) {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    newResponse = await contract.lastResponse();
-    console.log(".");
-  }
+    // loop and sleep by 1000ms, and keep printing `lastResponse` in the contract.
+    let lastResponse = await contract.lastResponse();
+    let newResponse = lastResponse;
 
-  console.log(`Image generation completed, image URL: ${newResponse}`)
+    // print w/o newline
+    console.log("Waiting for response: ");
+    while (newResponse === lastResponse) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        newResponse = await contract.lastResponse();
+        console.log("🔨...🔨...🔨...🔨...🔨...🔨...🔨...🔨");
+    }
+
+    console.log(`Image generation completed, image URL: ${newResponse}`)
 
 }
 
 async function getUserInput(): Promise<string | undefined> {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  const question = (query: string): Promise<string> => {
-    return new Promise((resolve) => {
-      rl.question(query, (answer) => {
-        resolve(answer)
-      })
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     })
-  }
 
-  try {
-    const input = await question("Enter an image description: ")
-    rl.close()
-    return input
-  } catch (err) {
-    console.error('Error getting user input:', err)
-    rl.close()
-  }
+    const question = (query: string): Promise<string> => {
+        return new Promise((resolve) => {
+            rl.question(query, (answer) => {
+                resolve(answer)
+            })
+        })
+    }
+
+    try {
+        const input = await question("Enter an image description: ")
+        rl.close()
+        return input
+    } catch (err) {
+        console.error('Error getting user input:', err)
+        rl.close()
+    }
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
